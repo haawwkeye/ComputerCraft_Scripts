bfa2a33c11d13cc013dc7aeb86890e581eb4f373
Hawk <30607615+haawwkeye@users.noreply.github.com>
2022-07-23 23:03:24 -0400

    Change install to a function

--- Turtle/TurtleAPIUpdater.lua
+++ Turtle/TurtleAPIUpdater.lua
@@ -2,6 +2,6 @@
 local updateApi = {}
 
 updateApi._Version = "0.0.1";
-updateApi.install = "wget https://raw.githubusercontent.com/haawwkeye/ComputerCraft_Scripts/Ame/Turtle/InstallTurtleAPI.lua InstallTurtleAPI.lua";
+updateApi.install = function() shell.run('wget https://raw.githubusercontent.com/haawwkeye/ComputerCraft_Scripts/Ame/Turtle/InstallTurtleAPI.lua InstallTurtleAPI.lua') end;
 
 return updateApi;
