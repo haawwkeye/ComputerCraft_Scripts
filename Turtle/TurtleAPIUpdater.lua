72078f992ff2010de081b051db9cef9af3996caf
Hawk <30607615+haawwkeye@users.noreply.github.com>
2022-07-23 22:58:09 -0400

    Temp stuff idk how ima actually do this lol

--- Turtle/TurtleAPIUpdater.lua
+++ Turtle/TurtleAPIUpdater.lua
@@ -1,1 +1,7 @@
 
+local updateApi = {}
+
+updateApi._Version = "0.0.1";
+updateApi.install = "wget https://raw.githubusercontent.com/haawwkeye/ComputerCraft_Scripts/Ame/Turtle/InstallTurtleAPI.lua InstallTurtleAPI.lua";
+
+return updateApi;
